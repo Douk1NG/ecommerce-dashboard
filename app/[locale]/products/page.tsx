1f895c5b287a1a9175484620134816b0e92ae638
aLxVa <dialexanderx@gmail.com>
2025-01-19 20:34:21 -0500

    fix products page

--- app/[locale]/products/page.tsx
+++ app/[locale]/products/page.tsx
@@ -1,5 +1,5 @@
 import { PageProps } from "@/types/layout"
-import Layout from "@/modules/components/products/products"
+import Layout from "@/modules/components/products/index"
 
 export default async function Page(props: PageProps) {
     const { locale } = await props.params
